175f47ddaf7ec3b377fa06dabd514c16ca002ee7
acatalina <accarrasco@gmail.com>
2017-04-27 19:05:13 +0100

    [feat-4] refactor to return a formatted user when posting new user

--- controllers/postNewUserController.js
+++ controllers/postNewUserController.js
@@ -1,10 +1,22 @@
 const Users = require('../models/Users');
 const UserLanguages = require('../models/UserLanguages');
+const Languages = require('../models/Languages');
+const Levels = require('../models/Levels');
 const async = require('async');
 
-function postNewUserController (req, res, next) {
-  let newUser = req.body;    
+function getLanguages (done) {
+  Languages.find({}, (err, languages) => {
+    return err ? done(err) : done(null, languages);
+  });
+}
 
+function getLevels (done) {
+  Levels.find({}, (err, levels) => {
+    return err ? done(err) : done(null, levels);
+  });
+}
+
+function createUser (newUser, done) {
   async.map(newUser.userLanguages, (userLanguage, next) => {
     let language = Object.keys(userLanguage);
     let level = userLanguage[language[0]];
@@ -14,7 +26,7 @@ function postNewUserController (req, res, next) {
       return err ? next(err) : next(null, userLanguage);
     });
   }, (err, userLanguages) => {
-    if (err) return next(err);
+    if (err) return done(err);
     let createdUserLanguages = userLanguages;
     
     newUser.userLanguages = userLanguages.reduce((res, userLanguage) => {
@@ -26,9 +38,42 @@ function postNewUserController (req, res, next) {
     
     newUser.save((err, createdUser) => {
       createdUser.userLanguages = createdUserLanguages;
-
-      return err ? next(err) : res.status(201).send({user: createdUser});
+      done(null, createdUser);
     });
+  });
+}
+
+function composeNewUser (info) {
+  info.createdUser.UserLanguages = info.createdUser.userLanguages.map(userLanguage => {
+    info.languages.forEach((language) => {
+      if (language._id.toString() === userLanguage.language.toString()) {
+        userLanguage.language = language.name;
+      }
+    });
+    
+    info.levels.forEach((level) => {
+      if (level._id.toString() === userLanguage.level.toString()) {
+        userLanguage.level = level.name;
+      }
+    });
+
+    return userLanguage;
+  });
+  
+  return info.createdUser;
+}
+
+function postNewUserController (req, res, next) {
+  let newUser = req.body;    
+  
+  async.parallel({
+    createdUser: createUser.bind(null, newUser),
+    languages: getLanguages,
+    levels: getLevels
+  }, (err, result) => {
+    let composedNewUser = composeNewUser(result);
+    
+    return err ? next(err) : res.status(201).send({user: composedNewUser});
   });
 }
 
